306e8ff94d49b4dc53f7dc80a0b322cf49b38b86
Daniel <danielsanjuan034@gmail.com>
2022-07-05 22:46:17 -0400

    first commit

---
+++ assets/js/scriptimg.js
@@ -0,0 +1,11 @@
+function marcar(){
+
+let imgElement = document.querySelector('img');
+
+if(imgElement.className == 'no.border'){
+    imgElement.className = 'red.border'
+}else{
+    imgElement.className = 'no.border'
+}
+
+}
---
+++ assets/js/scriptpassword.js
@@ -0,0 +1,11 @@
+function ingresar() {
+
+value1 = document.querySelector('select1').value
+value2 = document.querySelector('select2').value
+value3 = document.querySelector('select3').value
+
+if((value1 + value2 + value3) = 11){
+par = document.querySelector('#texto')
+par.innerHTML = 'Password 1 correcta'
+}
+}
